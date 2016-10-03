d6728a4ca0dd0e31ff962d8a4496ce83d01ff056
Janet Mohler <janetmohler82@gmail.com>
2016-10-03 13:43:37 -0700

    Added user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var substract = function(number1, mumber2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+
+alert(add(number1, number2));
+});
